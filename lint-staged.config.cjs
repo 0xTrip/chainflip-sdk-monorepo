2f9d4681637256b79dc5006b16f71077081c7dc2
Zoheb Salam <zoheb@chainflip.io>
2023-02-21 15:53:32 +0200

    test package.json

---
+++ lint-staged.config.cjs
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
+const { ESLint } = require("eslint");
+
+module.exports = {
+  "*.{ts,js}": [
+    async (files) => {
+      const eslint = new ESLint();
+      const isIgnored = await Promise.all(
+        files.map((file) => eslint.isPathIgnored(file))
+      );
+      const filteredFiles = files.filter((_, i) => !isIgnored[i]);
+      return [`eslint --max-warnings=0 ${filteredFiles.join(" ")}`];
+    },
+    "prettier --check",
+  ],
+  "*.{yaml,yml}": ["prettier --check"],
+};
