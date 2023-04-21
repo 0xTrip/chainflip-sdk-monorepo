b60ebc2bbb6a98508d0f0ab779889652c6d3bab6
Andrew Dibble <andrew@chainflip.io>
2023-04-21 19:19:18 +0200

    fix(swap): node 16

--- packages/swap/src/index.ts
+++ packages/swap/src/index.ts
@@ -1,14 +1,13 @@
 import 'dotenv/config';
 import { Server } from 'http';
-// import start from './processor';
+import start from './processor';
 import app from './server';
 import { handleExit } from './utils/function';
 import logger from './utils/logger';
 
-// start();
+start();
 app.listen(
   Number.parseInt(process.env.SWAPPING_APP_PORT as string, 10) || 8080,
-  '0.0.0.0',
   // eslint-disable-next-line func-names
   function (this: Server) {
     logger.info('server listening', { address: this.address() });
@@ -20,7 +19,3 @@ app.listen(
 process.on('exit', (code) => {
   logger.info(`process exiting with code "${code}"`);
 });
-
-setInterval(() => {
-  logger.info('the event loop is not blocked');
-}, 60000);
